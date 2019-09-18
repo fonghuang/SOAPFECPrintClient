4ff41c038ba351e233b5e8870841669228d1478a
fonghuang <43801973+fonghuang@users.noreply.github.com>
2019-09-18 16:48:46 -0400

    Add files via upload

---
+++ SOAPFECPrintClient.java
@@ -0,0 +1,108 @@
+import javax.xml.soap.MessageFactory;
+import javax.xml.soap.MimeHeaders;
+import javax.xml.soap.SOAPBody;
+import javax.xml.soap.SOAPConnection;
+import javax.xml.soap.SOAPConnectionFactory;
+import javax.xml.soap.SOAPElement;
+import javax.xml.soap.SOAPEnvelope;
+import javax.xml.soap.SOAPException;
+import javax.xml.soap.SOAPMessage;
+import javax.xml.soap.SOAPPart;
+
+public class SOAPFECPrintClient {
+
+    // SAAJ - SOAP Client Testing
+    public static void main(String args[]) {
+        /*
+            The example below requests from the Web Service at:
+             https://www.w3schools.com/xml/tempconvert.asmx?op=CelsiusToFahrenheit
+
+
+            To call other WS, change the parameters below, which are:
+             - the SOAP Endpoint URL (that is, where the service is responding from)
+             - the SOAP Action
+
+            Also change the contents of the method createSoapEnvelope() in this class. It constructs
+             the inner part of the SOAP envelope that is actually sent.
+         */
+        String soapEndpointUrl = "https://efoservices.fec.gov/webprint/services/print";
+        String soapAction = "http://service.webprint.efo.fec.gov/";
+
+        callSoapWebService(soapEndpointUrl, soapAction);
+    }
+
+    private static void createSoapEnvelope(SOAPMessage soapMessage) throws SOAPException {
+        SOAPPart soapPart = soapMessage.getSOAPPart();
+
+        String myNamespace = "myNamespace";
+        String myNamespaceURI = "http://service.webprint.efo.fec.gov/";
+
+        // SOAP Envelope
+        SOAPEnvelope envelope = soapPart.getEnvelope();
+        envelope.addNamespaceDeclaration(myNamespace, myNamespaceURI);
+
+            /*
+            Constructed SOAP Request Message:
+            <SOAP-ENV:Envelope xmlns:SOAP-ENV="http://schemas.xmlsoap.org/soap/envelope/" xmlns:myNamespace="https://www.w3schools.com/xml/">
+                <SOAP-ENV:Header/>
+                <SOAP-ENV:Body>
+                    <myNamespace:CelsiusToFahrenheit>
+                        <myNamespace:Celsius>100</myNamespace:Celsius>
+                    </myNamespace:CelsiusToFahrenheit>
+                </SOAP-ENV:Body>
+            </SOAP-ENV:Envelope>
+            */
+
+        // SOAP Body
+        SOAPBody soapBody = envelope.getBody();
+        SOAPElement soapBodyElem = soapBody.addChildElement("print", myNamespace);
+        SOAPElement soapBodyElem1 = soapBodyElem.addChildElement("arg0");
+        soapBodyElem1.addTextNode("5785a38a36a1c3ab9a9e76dc7c1a43ba58c7FTPz");
+        SOAPElement soapBodyElem2 = soapBodyElem.addChildElement("arg1");
+        soapBodyElem2.addTextNode("fong.huang@pasoftsolutions.com");
+        SOAPElement soapBodyElem3 = soapBodyElem.addChildElement("arg2");
+        soapBodyElem3.addTextNode("");
+
+    }
+
+    private static void callSoapWebService(String soapEndpointUrl, String soapAction) {
+        try {
+            // Create SOAP Connection
+            SOAPConnectionFactory soapConnectionFactory = SOAPConnectionFactory.newInstance();
+            SOAPConnection soapConnection = soapConnectionFactory.createConnection();
+
+            // Send SOAP Message to SOAP Server
+            SOAPMessage soapResponse = soapConnection.call(createSOAPRequest(soapAction), soapEndpointUrl);
+
+            // Print the SOAP Response
+            System.out.println("Response SOAP Message:");
+            soapResponse.writeTo(System.out);
+            System.out.println();
+
+            soapConnection.close();
+        } catch (Exception e) {
+            System.err.println("\nError occurred while sending SOAP Request to Server!\nMake sure you have the correct endpoint URL and SOAPAction!\n");
+            e.printStackTrace();
+        }
+    }
+
+    private static SOAPMessage createSOAPRequest(String soapAction) throws Exception {
+        MessageFactory messageFactory = MessageFactory.newInstance();
+        SOAPMessage soapMessage = messageFactory.createMessage();
+
+        createSoapEnvelope(soapMessage);
+
+        MimeHeaders headers = soapMessage.getMimeHeaders();
+        headers.addHeader("SOAPAction", soapAction);
+
+        soapMessage.saveChanges();
+
+        /* Print the request message, just for debugging purposes */
+        System.out.println("Request SOAP Message:");
+        soapMessage.writeTo(System.out);
+        System.out.println("\n");
+
+        return soapMessage;
+    }
+
+}
